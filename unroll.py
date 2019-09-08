3f993381f6b72a4b279828ef67fe77a640ca09e9
João Pedro <jpedrodeamorim@gmail.com>
2019-09-08 19:33:24 -0300

    first attempt at doing something...

---
+++ unroll.py
@@ -0,0 +1,18 @@
+import time
+import os
+import mmap
+import signal
+import posix_ipc
+import sys
+import struct
+
+def unroll(args, func, method, res):
+    if method == 'proc':
+
+    elif method == 'thre':
+
+    else:
+        print("method " + method + " unknown!")
+        return
+
+res = []
