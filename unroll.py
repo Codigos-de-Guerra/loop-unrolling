eb62e6202ca940dc2e32ac19168150ac73832e09
Daniel Guerra <danieloguerra@ufrn.edu.br>
2019-09-09 13:59:56 -0300

    Added three functions. Read and create 2 input matrices; calculate the resulting matrix; and save in a new file the resulting matrix. All calculations done right! Argument passage also great! Passing, receiving and returning all functional matrices are functional.

--- unroll.py
+++ unroll.py
@@ -1,18 +1,77 @@
-import time
-import os
-import mmap
-import signal
-import posix_ipc
+#!/usr/bin/env python3
+
+# import time
+# import os
+# import mmap
+# import signal
+# import posix_ipc
 import sys
-import struct
+# import struct
+import vish
 
-def unroll(args, func, method, res):
-    if method == 'proc':
+try:
+    A_file = sys.argv[1]
+    B_file = sys.argv[2]
 
-    elif method == 'thre':
+except:
+    raise Exception("Not enough matrixes passed down as arguments!")
 
-    else:
-        print("method " + method + " unknown!")
-        return
+def create_matrix(file1, file2):# {{{
+    with open(A_file, "r") as file:
+        matrix_A = []
+        for line in file:
+            matrix_A.append(list(map(lambda x: int(x), line.split())))
 
-res = []
+
+    with open(B_file, "r") as file:
+        matrix_B = []
+        for line in file:
+            matrix_B.append(list(map(lambda x: int(x), line.split())))
+
+    return matrix_A, matrix_B# }}}
+
+def calc_matrix(matrixA, matrixB, size):# {{{
+    res = []
+    for i in range(size):
+        line = []
+        for j in range(size):
+            element = 0
+            for l in range(size):
+                element += matrixA[i][l] * matrixB[l][j]
+            line.append(element)
+        res.append(line)
+
+    return res# }}}
+
+def printToFile(result, filename):# {{{
+    with open(filename, 'w') as output:
+        buf = ""
+        for line in range(len(result)):
+            for col in range(len(result)):
+                buf += str(result[line][col]) + " "
+            buf += "\n"
+        output.write(buf)
+
+    print("Output generated! It can be found at '{}'".format(filename))# }}}
+
+A, B = create_matrix(A_file, B_file)
+matrix_size = len(A)
+
+if len(A) != len(B) or len(A[0]) != len(B[0]):
+    raise Exception("Matrixes read can't be multiplied! Try others.")
+
+C = calc_matrix(A, B, matrix_size)
+
+# print(A)
+# print(B)
+
+print("Matriz Resultado:")
+# buf = ""
+# for i in C:
+#     buf += "| "
+#     for j in i:
+#         buf += str(j) + " "
+#     buf += '|\n'
+
+# print(buf)
+printToFile(C, "data/{}x{}/output.dat".format(matrix_size, matrix_size))
