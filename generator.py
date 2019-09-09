9f9ae5beb093b6df41449d2c82202588c012c6a0
Daniel Guerra <danieloguerra@ufrn.edu.br>
2019-09-09 13:50:05 -0300

    Created a generic matrix generator. Used to create several randomic matrices with required sizes.

---
+++ generator.py
@@ -0,0 +1,29 @@
+from random import randint
+import sys
+
+def generator(size):# {{{
+    filename = "data/{}x{}/".format(size,size)
+
+    with open(filename+"X.dat", 'w') as output:
+        buf = ""
+        for line in range(size):
+            for col in range(size):
+                rand_value = randint(0, size*size + 5)
+                buf += str(rand_value) + " "
+            buf += '\n'
+        output.write(buf)
+
+    with open(filename+"Y.dat", 'w') as output:
+        buf = ""
+        for line in range(size):
+            for col in range(size):
+                rand_value = randint(0, size*size + 5)
+                buf += str(rand_value) + " "
+            buf += '\n'
+        output.write(buf)
+# }}}
+
+for num in sys.argv[1:]:
+    generator(int(num))
+    print("Two input files for {}x{} matrixes been created!".format(num,num))
+    # generator(int(sys.argv[1]))
