3ae31bab0bf3bc128b3e3ec6f19507c456ec97fa
Daniel Guerra <danieloguerra@ufrn.edu.br>
2019-09-09 19:57:01 -0300

    Minor modification attending to python tendencies

--- __main__.py
+++
@@ -1,5 +0,0 @@
-import sequencial
-import unroll
-
-def __main__():
-    print("oi")
---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    import sequencial
+    import unroll
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Hell no nigga")
+    else:
+        main()
