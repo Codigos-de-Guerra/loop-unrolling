dd4e14d15183c1df16993817baa8a638986dee32
Daniel Guerra <danieloguerra@ufrn.edu.br>
2019-09-09 20:57:01 -0300

    Main better structured and cleaner.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import sys
 
 def main():
-    import sequencial
+    import sequential
     import unroll
+
+    # par_sum = par_mul = seq_sum = seq_mul = (b-a).total_seconds()
+
+    par_sum, par_mul = unroll.parallel()
+    seq_sum, seq_mul = sequential.seq()
+
+    print("Sequential Sum time: {} milliseconds".format(seq_sum))
+    print("Sequential Multiplication time: {} milliseconds".format(seq_mul))
+
+    print("Thread Sum time: {} milliseconds.".format(par_sum))
+    print("Thread Multiplication time: {} milliseconds.".format(par_mul))
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
